b5b9be1fd69eb799b5e01b3061c23bd02b40edad
Derek Wright <git@dwwright.net>
2007-03-30 23:45:53 +0000

    #49007: Fix all the UI bugs and weird behavior about enabling and disabling signups per-node-type and per-node. Includes jQuery code to further cleanup the UI (by hiding irrelevent settings).

---
+++ signup.js
@@ -0,0 +1,19 @@
+/* $Id$ */
+
+/**
+ * On a node form, if the "Allow signups" radios are set to 1
+ * ('Enabled'), then show the other signup-specific settings,
+ * otherwise, hide them.
+ */
+Drupal.signupShowSettingsAutoAttach = function () {
+  $('div.signup-allow-radios input[@type=radio]').click(function () {
+    $('div.signup-node-settings')[['hide', 'show', 'hide'][this.value]]();
+  });
+}
+
+// Global killswitch.
+$(function() {
+  if (Drupal.jsEnabled) {
+    Drupal.signupShowSettingsAutoAttach();
+  }
+});
